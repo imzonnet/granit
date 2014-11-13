92ae69917b2881ab0a8d41d12a696716e1856c63
Duc Anh <vnzacky39@gmail.com>
2014-11-13 22:14:33 +0700

    Environment: ducanh

--- app/config/ducanh/database.php
+++ app/config/ducanh/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
             'host'      => 'localhost',
-            'database'  => 'lv_haivl',
+            'database'  => 'lv_doptor',
             'username'  => 'root',
             'password'  => '',
 			'charset'   => 'utf8',
