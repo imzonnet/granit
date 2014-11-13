4cce33266bad0e581a7475b65163143bb71fbf3c
Duc Anh <vnzacky39@gmail.com>
2014-11-13 22:16:00 +0700

    đâ

--- app/config/ducanh/database.php
+++ app/config/ducanh/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
             'host'      => 'localhost',
-            'database'  => 'lv_doptor',
+            'database'  => 'lv_granit',
             'username'  => 'root',
             'password'  => '',
 			'charset'   => 'utf8',
