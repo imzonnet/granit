7383fa0377372a36021e502e49949632577158d8
Duc Anh <vnzacky39@gmail.com>
2014-11-18 17:09:43 +0700

    Color up

---
+++ app/components/Stones/validation/ColorValidator.php
@@ -0,0 +1,43 @@
+<?php
+namespace Components\Stones\Validation;
+
+use Services\Validation\Validator as Validator;
+
+class ColorValidator extends Validator{
+
+	/**
+     * Default rules
+     * @var array
+     */
+    protected $rules = array(
+        'name'     		=> 'required|regex:/^[a-zA-Z0-9\-\s\?\{\}\(\)ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ]+$/',
+        'hexcode' 		=> 'required|min:7|max:7',
+    );
+
+    /**
+     * Default rules for update
+     * @var array
+     */
+    protected $updateRules = array(
+        'name'     		=> 'required|regex:/^[a-zA-Z0-9\-\s\?\{\}\(\)ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ]+$/',
+        'hexcode'       => 'required|min:7|max:7',
+    );
+
+    /**
+     * Messages for validation
+     * @var array
+     */
+    protected $message = array(
+        'hexcode'   => 'The hexcode must have an exact length of 7.'
+    );
+
+    public function validateForCreation($input)
+    {
+        return $this->validate($input, $this->rules, $this->message);
+    }
+    public function validateForUpdate($input)
+    {
+        return $this->validate($input, $this->updateRules, $this->message);
+    }
+
+}
