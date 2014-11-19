2b2db8a53d6900ac7b83c1c5bb30af343bc4b0b1
conghieu711 <conghieu20@gmail.com>
2014-11-19 14:43:20 +0700

    re-commit

--- app/components/Stones/routes.php
+++ app/components/Stones/routes.php
@@ -4,3 +4,4 @@ Route::resource('/backend/stones/icons', 'Components\Stones\Controllers\Backend\
 Route::resource('/backend/stones/icon-categories', 'Components\Stones\Controllers\Backend\IconCategoriesController');
 Route::resource('/backend/stones/colors', 'Components\Stones\Controllers\Backend\ColorsController');
 Route::resource('/backend/stones/fonts', 'Components\Stones\Controllers\Backend\FontsController');
+
