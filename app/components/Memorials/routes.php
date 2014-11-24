d1b10e8a489e03a92f9efa45e78113229b4eeb56
Duc Anh <vnzacky39@gmail.com>
2014-11-24 17:24:14 +0700

    Fix namespace

--- app/components/Memorials/routes.php
+++ app/components/Memorials/routes.php
@@ -1,10 +1,10 @@
 <?php
 
 // Backend
-Route::resource('/backend/memorials','Components\Memorials\Controllers\backend\MemorialsController');
-Route::resource('/backend/memorial.guestbooks','Components\Memorials\Controllers\backend\GuestbooksController');
-Route::resource('/backend/memorial.media','Components\Memorials\Controllers\backend\MediaController');
-Route::resource('/backend/memorial.users','Components\Memorials\Controllers\backend\UsersController');
+Route::resource('/backend/memorials','Components\Memorials\Controllers\Backend\MemorialsController');
+Route::resource('/backend/memorial.guestbooks','Components\Memorials\Controllers\Backend\GuestbooksController');
+Route::resource('/backend/memorial.media','Components\Memorials\Controllers\Backend\MediaController');
+Route::resource('/backend/memorial.users','Components\Memorials\Controllers\Backend\UsersController');
 
 // Frontend
 Route::resource('/memorial','Components\Memorials\Controllers\MemorialsController', ['only' => ['index', 'show']]);
