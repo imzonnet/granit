39b4edb551293a561cf885f463ebbe1f3c32e778
Duc Anh <vnzacky39@gmail.com>
2014-12-22 17:40:57 +0700

    Up

---
+++ app/components/Products/models/Color.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Components\Products\Models;
+
+use App,
+    Str;
+use Robbo\Presenter\PresentableInterface;
+use Components\Products\Presenters\ColorPresenter;
+use Components\Products\Models\ProductColor;
+
+class Color extends \Eloquent implements PresentableInterface {
+
+    protected $table = 'granit_product_colors';
+    protected $fillable = array('name', 'icon');
+    protected $guarded = array('id');
+
+    /**
+     * Relationship
+     */
+    public function product_color() {
+        return $this->hasMany('Components\Products\Models\ProductColor', 'color_id', 'id');
+    }
+
+    /**
+     * When creating a category, run the attributes through a validator first.
+     * @param array $attributes
+     * @return void
+     */
+    public static function create(array $attributes = array()) {
+        App::make('Components\\Products\\Validation\\ColorValidator')->validateForCreation($attributes);
+
+        return parent::create($attributes);
+    }
+
+    /**
+     * When creating a category, run the attributes through a validator first.
+     * @param array $attributes
+     * @return void
+     */
+    public function update(array $attributes = array()) {
+        App::make('Components\\Products\\Validation\\ColorValidator')->validateForUpdate($attributes);
+
+        return parent::update($attributes);
+    }
+
+    /**
+     * Get all product categories
+     * @return array
+     */
+    public static function all_colors() {
+        $colors = array();
+        foreach (Color::all() as $color) {
+            $colors[$color->id] = $color->name;
+        }
+        return $colors;
+    }
+
+    /**
+     * Get all the statuses available for a post
+     * @return array
+     */
+    public static function all_status() {
+        return array(
+            'published' => 'Publish',
+            'unpublished' => 'Unpublish',
+            'drafted' => 'Draft',
+            'archived' => 'Archive'
+        );
+    }
+
+    /**
+     * Initiate the presenter class
+     */
+    public function getPresenter() {
+        return new ColorPresenter($this);
+    }
+
+}
---
+++ app/components/Products/presenters/ColorPresenter.php
@@ -0,0 +1,9 @@
+<?php namespace Components\Products\Presenters;
+
+use Robbo\Presenter\Presenter;
+
+class ColorPresenter extends Presenter
+{
+    
+
+}
